9fdd156085364bb0b7667ea0caf83679094d996e
Minh Chieng <minhvc.mobile@gmail.com>
2019-06-15 12:03:22 +0700

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DanhsachNhanvienComponent } from './danhsach-nhanvien/danhsach-nhanvien.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DanhsachNhanvienComponent,
+    HeaderComponent,
+    NavbarComponent,
+    MainContentComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
